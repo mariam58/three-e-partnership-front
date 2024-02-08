65322baa5d32b754920e8eec33874b746393094a
nino <ninosopoian@gmail.com>
2024-02-08 16:37:00 +0400

    add burger-rotation on click in js

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,7 @@
 function burger() {
-        const burgerMenu = document.querySelector('#burger-menu');
-        const navigation = document.querySelector('#navigation');
-        const logInButton = document.querySelector('#actions');
-    
+        const burgerMenu = document.querySelector('#burger-menu');    
         burgerMenu.addEventListener('click', ()=> {
             burgerMenu.classList.toggle('active');
-            navigation.classList.toggle('burger-active')
-            logInButton.classList.toggle('actions-active')
         })
     }
     burger();
