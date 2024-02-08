e6616c7cb26b98c9e67166b3405a6e0b9c71d177
nino <ninosopoian@gmail.com>
2024-02-08 18:54:02 +0400

    add  acordion for mobile dropdowns

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,24 @@
+const burgerMenu = document.querySelector('#burger-menu');    
+// const dropdown = document.querySelectorAll('.dropdown')
+
 function burger() {
-        const burgerMenu = document.querySelector('#burger-menu');    
         burgerMenu.addEventListener('click', ()=> {
             burgerMenu.classList.toggle('active');
+
         })
     }
     burger();
     
+    let acordionWrap = document.querySelectorAll(".step");
+    // for (let item of acordionWrap) {
+    //   item.addEventListener("click", function (e) {
+    //     this.classList.toggle('active');
+    //     // e.currentTarget.classList.toggle('active')
+    //   });
+    // }
+    
+    acordionWrap.forEach(function (elemenet) {
+      elemenet.addEventListener("click", function () {
+        this.classList.toggle("active");
+      });
+    });
