e25521959b8ceb823481e6a7571a2f05c61abebd
nino <ninosopoian@gmail.com>
2024-02-08 16:25:03 +0400

    add burger-menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
-
+function burger() {
+        const burgerMenu = document.querySelector('#burger-menu');
+        const navigation = document.querySelector('#navigation');
+        const logInButton = document.querySelector('#actions');
+    
+        burgerMenu.addEventListener('click', ()=> {
+            burgerMenu.classList.toggle('active');
+            navigation.classList.toggle('burger-active')
+            logInButton.classList.toggle('actions-active')
+        })
+    }
+    burger();
+    
