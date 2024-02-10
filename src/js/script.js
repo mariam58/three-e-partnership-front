d5393820bc2ba4e98ede06221211d3d2d6aec93f
nino <ninosopoian@gmail.com>
2024-02-10 22:06:58 +0400

    add mobile menu navigation. add new styles in input.css  fixed js file

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,21 @@
 const burgerMenu = document.querySelector('#burger-menu');
-const mobileNav = document.querySelector('#mobile-navigation')    
+const megaMenu = document.getElementById('menu-container')
+// const mobileNav = document.querySelector('#mobile-navigation')    
 // const mobileDropdown = document.querySelectorAll('.mobile-dropdown');
 const accordionItem = document.querySelectorAll(".step");
 
 function burger() {
         burgerMenu.addEventListener('click', ()=> {
-            burgerMenu.classList.toggle('active');
-            document.body.classList.toggle('bg-black')
-           if(burgerMenu.classList.contains('active')){
-            mobileNav.classList.add('mobile-nav', 'acordion-wrap')
-           }
-           else {
-            mobileNav.classList.remove('mobile-nav', 'acordion-wrap')
-           }
+            burgerMenu.classList.toggle('menu-btn');
+            megaMenu.classList.toggle('mega-menu-show');
+            document.body.classList.toggle('bg-black');
+            
+          //  if(burgerMenu.classList.contains('active')){
+          //   mobileNav.classList.add('mobile-nav', 'acordion-wrap')
+          //  }
+          //  else {
+          //   mobileNav.classList.remove('mobile-nav', 'acordion-wrap')
+          //  }
           
         })
     }
@@ -31,4 +34,62 @@ function burger() {
     });
 
  
+
+// // Get elements from the DOM
+// const menuBtn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.menu-container');
+// const dropdowns = document.querySelectorAll('.dropdown > div');
+// const subDropdowns = document.querySelectorAll('.sub-dropdown > div');
+
+// // Toggle variable
+// let menuOpen = false;
+
+// // Set click event to menu button
+// menuBtn.addEventListener('click', ()=>{
+//     // Toggle mega menu show class
+//     menu.classList.toggle('mega-menu-show');
+//     // If the menu open variable is false
+//     if(menuOpen===false){
+//         // Set the close icon to the menu button
+//         menuBtn.innerHTML=`
+//         <span class="material-symbols-outlined">
+//             close
+//         </span>
+//         `;
+//         // Set menu open to true
+//         menuOpen = true;
+//     }
+//     else{
+//         // Set the menu icon to the menu button
+//         menuBtn.innerHTML=`
+//         <span class="material-symbols-outlined">
+//             menu
+//         </span>
+//         `;
+//         // Set menu open to false
+//         menuOpen = false;
+//     }
+// });
+
+// // Select all dropdowns
+// dropdowns.forEach(dropdown=>{
+//     // Add click event
+//     dropdown.addEventListener("click", (e)=>{
+//         // Toggle dropdown menu show class
+//         dropdown.nextElementSibling.classList.toggle('menu-show');
+//         // Toggle icon rotated class
+//         dropdown.lastElementChild.classList.toggle('icon-rotated');
+//     });
+// });
+
+// // Select all sub dropdowns
+// subDropdowns.forEach(subDropdown=>{
+//     // Add click event
+//     subDropdown.addEventListener('click', (e)=>{
+//         // Toggle sub dropdown menu show class
+//         subDropdown.nextElementSibling.classList.toggle('sub-menu-show');
+//         // Toggle icon rotated class
+//         subDropdown.lastElementChild.classList.toggle('icon-rotated');
+//     });
+// });
 
