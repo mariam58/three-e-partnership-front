5532d8b9b5ed0cae7643b5f610609c984a930a69
nino <ninosopoian@gmail.com>
2024-02-14 14:33:23 +0400

    fix search styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -83,5 +83,9 @@ module.exports = {
       }],
     }
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss/plugin')(({ addVariant }) => {
+      addVariant('search-cancel', '&::-webkit-search-cancel-button');
+    }),
+  ],
 }
