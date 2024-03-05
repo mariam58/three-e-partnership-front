bddcdee86673968a2441462666c82d819002b62a
Mariam Nonikashvili <61709769+mariam58@users.noreply.github.com>
2024-03-05 18:37:29 +0400

    Revert "format detail-page; js; css"
    This reverts commit e0b934d16d70d6d5482156d43bc9591f6b9465d6.

--- src/js/script.js
+++ src/js/script.js
@@ -1,102 +1,127 @@
-const header = document.querySelector(".header");
-const burgerMenu = document.querySelector("#burger-menu");
-const dropdownMenu = document.getElementById("nav-container-wrapper");
+const header = document.querySelector('.header')
+const burgerMenu = document.querySelector('#burger-menu');
+const dropdownMenu = document.getElementById('nav-container-wrapper')
 const dropdownItem = document.querySelectorAll(".navigation-menu-item");
-const search = document.querySelector(".search-onclick");
-const overlayContant = document.getElementById("overlay");
+const search = document.querySelector('.search-onclick')
+const overlayContant = document.getElementById('overlay');
 const DesktopSearch = document.getElementById("desktop-search-btn");
+
 
 // logo
 const whiteLogo = document.querySelector(".logo-white");
 const blackLogo = document.querySelector(".logo-black");
 
+
+
 function burger() {
-  burgerMenu.addEventListener("click", () => {
-    burgerMenu.classList.toggle("menu-btn");
-    dropdownMenu.classList.toggle("dropdown-menu-show");
-    dropdownMenu.classList.toggle("h-0");
-    header.classList.toggle("bg-black");
-    // document.body.classList.toggle('bg-black');
-    document.body.classList.toggle("overflow-hidden");
+        burgerMenu.addEventListener('click', ()=> {
+            burgerMenu.classList.toggle('menu-btn');
+            dropdownMenu.classList.toggle('dropdown-menu-show');
+            dropdownMenu.classList.toggle('h-0')
+            header.classList.toggle('bg-black')
+            // document.body.classList.toggle('bg-black');
+            document.body.classList.toggle('overflow-hidden');
 
-    blackLogo.classList.toggle("hide-logo");
-    blackLogo.classList.toggle("hidden");
-    whiteLogo.classList.toggle("hidden");
-    DesktopSearch.classList.toggle("hidden");
-    dropdownMenu.classList.toggle("dropdown-menu-scroll");
-  });
-}
-burger();
+            blackLogo.classList.toggle('hide-logo');
+              blackLogo.classList.toggle('hidden');
+              whiteLogo.classList.toggle('hidden');
+              DesktopSearch.classList.toggle('hidden')
+              dropdownMenu.classList.toggle('dropdown-menu-scroll')
 
-window.addEventListener("resize", function () {
-  if (window.outerWidth > 1024) {
-    burgerMenu.classList.remove("menu-btn");
-    dropdownMenu.classList.remove("dropdown-menu-show");
-    header.classList.remove("bg-black");
+        })
+    }
+    burger();
+    
+   
 
-    // document.body.classList.remove('bg-black');
-    document.body.classList.remove("overflow-hidden");
-    blackLogo.classList.remove("hide-logo");
-    blackLogo.classList.remove("hidden");
-    whiteLogo.classList.add("hidden");
-    dropdownMenu.classList.remove("dropdown-menu-show");
-    dropdownMenu.classList.add("h-0");
-    DesktopSearch.classList.remove("hidden");
-    dropdownMenu.classList.remove("dropdown-menu-scroll");
-  }
-});
+ window.addEventListener('resize',function () {
+    if (window.outerWidth > 1024) {
+      burgerMenu.classList.remove('menu-btn');
+      dropdownMenu.classList.remove('dropdown-menu-show');
+      header.classList.remove('bg-black')
 
-dropdownItem.forEach(function (elemenet) {
-  elemenet.addEventListener("click", function () {
-    // this.classList.toggle("active");
-    let mobileDropdown = this.lastChild.previousElementSibling;
-    mobileDropdown.classList.toggle("hidden");
-    let arrowdown = this.querySelector(".navigation-title-icons");
-    arrowdown.classList.toggle("after:icon-up");
-  });
-});
+      // document.body.classList.remove('bg-black');
+      document.body.classList.remove('overflow-hidden');
+      blackLogo.classList.remove('hide-logo');
+        blackLogo.classList.remove('hidden');
+        whiteLogo.classList.add('hidden');
+        dropdownMenu.classList.remove('dropdown-menu-show');
+        dropdownMenu.classList.add('h-0')
+        DesktopSearch.classList.remove('hidden')
+        dropdownMenu.classList.remove('dropdown-menu-scroll')
+    }
+} )
 
-// close search popup
+    
+  dropdownItem.forEach(function (elemenet) {
+      elemenet.addEventListener("click", function () {
+        // this.classList.toggle("active");
+        let mobileDropdown = this.lastChild.previousElementSibling;
+        mobileDropdown.classList.toggle('hidden')
+        let arrowdown = this.querySelector('.navigation-title-icons')
+        arrowdown.classList.toggle('after:icon-up')
+      });
+    });
 
-// Detect all clicks on the document
-overlayContant.addEventListener("click", function (event) {
-  // If user clicks inside the element, do nothing
-  if (event.target.closest("#overlay-content")) return;
-  // If user clicks outside the element, hide it!
-  else overlayContant.classList.add("hidden");
-  document.body.classList.remove("overflow-hidden");
-});
+         // close search popup
 
-// open search popup
 
-function openSearch() {
-  overlayContant.classList.remove("hidden");
-  document.body.classList.add("overflow-hidden");
-}
-function closesearch() {
-  overlayContant.classList.add("hidden");
-  document.body.classList.remove("overflow-hidden");
-}
+    // Detect all clicks on the document
+    overlayContant.addEventListener("click", function(event) {
+      // If user clicks inside the element, do nothing
+      if (event.target.closest("#overlay-content"))
+      return 
+      // If user clicks outside the element, hide it!
+      else (overlayContant.classList.add("hidden"));
+      document.body.classList.remove('overflow-hidden');
 
-//  partners page
-const partnersBtn = document.getElementById("partners-btn");
-const partnersList = document.getElementById("partners-list");
+    });
 
-const associatedBtn = document.getElementById("associated-btn");
-const associatedList = document.getElementById("associated-list");
+     // open search popup
 
-function openPartners() {
-  partnersBtn.classList.toggle("bg-bg-main");
-  partnersBtn.classList.toggle("bg-colorsText-blue");
-  partnersList.classList.toggle("hidden");
-  partnersBtn.classList.toggle("text-white");
-  partnersBtn.classList.toggle("text-black");
-}
+    function openSearch() {
+      overlayContant.classList.remove('hidden');
+      document.body.classList.add('overflow-hidden');
+    }
+    function closesearch(){
+      overlayContant.classList.add('hidden');
+      document.body.classList.remove('overflow-hidden');
 
-function openassociated() {
-  associatedBtn.classList.toggle("bg-bg-main");
-  associatedBtn.classList.toggle("bg-colorsText-blue");
-  associatedBtn.classList.toggle("text-white");
-  associatedList.classList.toggle("hidden");
-  associatedBtn.classList.toggle("text-black");
-}
+    }
+
+    //  partners page
+const partnersBtn = document.getElementById('partners-btn')
+const partnersList = document.getElementById('partners-list')
+
+const associatedBtn = document.getElementById('associated-btn')
+const associatedList = document.getElementById('associated-list')
+
+
+
+    function openPartners(){
+      partnersBtn.classList.toggle('bg-bg-main')
+      partnersBtn.classList.toggle('bg-colorsText-blue')
+      partnersList.classList.toggle('hidden')
+      partnersBtn.classList.toggle('text-white')
+      partnersBtn.classList.toggle('text-black')
+
+
+    }
+
+    function openassociated(){
+      associatedBtn.classList.toggle('bg-bg-main')
+      associatedBtn.classList.toggle('bg-colorsText-blue')
+      associatedBtn.classList.toggle('text-white')
+      associatedList.classList.toggle('hidden')
+      associatedBtn.classList.toggle('text-black')
+
+    }
+
+    
+
+    
+
+
+
+ 
+
